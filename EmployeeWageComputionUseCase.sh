481f00dd3b009a6d0831244fad018a1d0adb911c
unknown <bhushan>
2021-01-08 18:22:42 +0530

    Added:dd part time employee and calculte wage

--- EmployeeWageComputionUseCase.sh
+++ EmployeeWageComputionUseCase.sh
@@ -1,15 +1,19 @@
 #! /bin/bash -x
 #define constant
-IS_PRESENT=1
+IS_PRESENT_FULL_TIME=1
+IS_PRESENT_PART_TIME=2
 EMP_RATE_PER_HR=20
 #DEFINE VARIBLE
-empCheck=$((RANDOM%2))
+empCheck1=$((RANDOM%3))
 empHrs=0
-if [ $IS_PRESENT -eq $empCheck ]
+if [ $IS_PRESENT_FULL_TIME -eq $empCheck1 ]
 then
         empHrs=8
+
+elif [ $IS_PRESENT_PART_TIME -eq $empCheck1 ]
+then
+        empHrs=4
 else
-        empHrs=0
+        emoHrs=0
 fi
-salary=$(( $empHrs * $EMP_RATE_PER_HR ))
-echo $salary
+salary=$(( $empHrs * $EMP_RATE_PER_HR  ))
