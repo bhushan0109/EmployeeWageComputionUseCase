f975eef62ccea0f7a9a7cfa0a20f8a0c8befc2ec
unknown <bhushan>
2021-01-08 18:28:34 +0530

    Added:solving using case statament used case

--- EmployeeWageComputionUseCase.sh
+++ EmployeeWageComputionUseCase.sh
@@ -4,16 +4,14 @@ IS_PRESENT_FULL_TIME=1
 IS_PRESENT_PART_TIME=2
 EMP_RATE_PER_HR=20
 #DEFINE VARIBLE
-empCheck1=$((RANDOM%3))
+empCheck=$(( RANDOM%3))
 empHrs=0
-if [ $IS_PRESENT_FULL_TIME -eq $empCheck1 ]
-then
-        empHrs=8
-
-elif [ $IS_PRESENT_PART_TIME -eq $empCheck1 ]
-then
-        empHrs=4
-else
-        emoHrs=0
-fi
+case $empCheck in
+        $IS_PRESENT_FULL_TIME)empHrs=8
+        ;;
+        $IS_PRESENT_PART_TIME)empHrs=4
+        ;;
+        *)empHrs=0
+        ;;
+esac
 salary=$(( $empHrs * $EMP_RATE_PER_HR  ))
