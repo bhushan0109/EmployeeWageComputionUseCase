014bf136dc0e59342ae6221f229f4a6d5c432ec9
unknown <bhushan>
2021-01-08 18:35:50 +0530

    Added:calculating the wages for month

--- EmployeeWageComputionUseCase.sh
+++ EmployeeWageComputionUseCase.sh
@@ -3,9 +3,13 @@
 IS_PRESENT_FULL_TIME=1
 IS_PRESENT_PART_TIME=2
 EMP_RATE_PER_HR=20
+WORKING_DAYS=20
 #DEFINE VARIBLE
-empCheck=$(( RANDOM%3))
-empHrs=0
+totalSalary=0
+for ((i=0;i<$WORKING_DAYS;i++))
+do
+#DEFINE VARIBLE
+empCheck=$((RANDOM%3))
 case $empCheck in
         $IS_PRESENT_FULL_TIME)empHrs=8
         ;;
@@ -15,3 +19,5 @@ case $empCheck in
         ;;
 esac
 salary=$(( $empHrs * $EMP_RATE_PER_HR  ))
+totalSalary=$(($totalSalary + $salary))
+done
